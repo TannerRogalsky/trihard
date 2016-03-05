992698050351a5d2010dde9ec6c2df9c5e94aa41
Tanner Rogalsky <the.t.dawg@gmail.com>
2016-03-05 15:22:06 -0500

    windows changes

--- src/init.lua
+++ src/init.lua
@@ -2,15 +2,15 @@ local _NAME = ...
 
 local function getLibrarySuffix()
   local os = love.system.getOS()
-  if os == 'OS X' then return 'so'
-  elseif os == 'Windows' then return 'dll'
+  if os == 'OS X' then return '.so'
+  elseif os == 'Windows' then return '-' .. jit.arch .. '.dll'
   else error('Your operating system is not supported yet.')
   end
 end
 
 local function loadPoly2Tri()
   local path = love.filesystem.getSourceBaseDirectory() .. '/' .. _NAME:gsub('%.', '/'):gsub('^.*/', '')
-  return assert(package.loadlib(path .. '/poly2tri.' .. getLibrarySuffix(), 'luaopen_poly2tri'))()
+  return assert(package.loadlib(path .. '/poly2tri' .. getLibrarySuffix(), 'luaopen_poly2tri'))()
 end
 
 return loadPoly2Tri()
